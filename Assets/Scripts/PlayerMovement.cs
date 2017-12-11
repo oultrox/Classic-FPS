e645d4becee471e9ecfc144932d629506fb8c88e
oultrox <jesus-perez95@hotmail.com>
2017-12-11 13:06:41 -0300

    [Upload] Initial project with the next feautres
    - Player movement
    - Player rotation
    - Player jump system
    - Head bobbling
    and more is coming!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private float playerWalkingSpeed = 5f;
+    [SerializeField] private float playerRunningSpeed = 15f;
+    [SerializeField] private float jumpStrength = 20f;
+    [SerializeField] private float verticalRotationLimit = 80f;
+    
+    private CharacterController controller;
+    private float verticalRotation;
+    private float forwardMovement;
+    private float sideAwaysMovement;
+    private Vector3 playerMovement;
+    private float verticalVelocity;
+
+
+    // Use this for initialization
+	void Awake ()
+    {
+        controller = this.GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        verticalRotation = 0;
+        playerMovement = Vector3.zero;
+	}
+
+    private void Update()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalRotation, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y");
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation,0, 0);
+  
+        //Movement
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            forwardMovement = Input.GetAxis("Vertical") * playerRunningSpeed;
+            sideAwaysMovement = Input.GetAxis("Horizontal") * playerRunningSpeed;
+        }
+        else
+        {
+            forwardMovement = Input.GetAxis("Vertical") * playerWalkingSpeed;
+            sideAwaysMovement = Input.GetAxis("Horizontal") * playerWalkingSpeed;
+        }
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            verticalVelocity = jumpStrength;
+        }
+
+        playerMovement.x = sideAwaysMovement;
+        playerMovement.y = verticalVelocity;
+        playerMovement.z = forwardMovement;
+        controller.Move(transform.rotation * playerMovement * Time.deltaTime);
+    }
+}
