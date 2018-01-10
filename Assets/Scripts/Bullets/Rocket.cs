da280ee189f4495993ce2f4539e1e45fb062ecf3
oultrox <jesus-perez95@hotmail.com>
2018-01-10 18:15:17 -0300

    [WIP] Rocket LAUNCHER!

---
+++ Assets/Scripts/Bullets/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] private GameObject explosionPrefab;
+
+    [SerializeField] private float radius;
+    [SerializeField] private int damage;
+    [SerializeField] private float lifeTime;
+    [SerializeField] private LayerMask layerMask;
+
+    private float lifeCounter;
+
+	void Start () {
+        lifeCounter = 0;
+	}
+	
+	void Update ()
+    {
+        lifeTime += Time.deltaTime;
+        if (lifeCounter >= lifeTime)
+        {
+            Explode();
+        }
+	}
+
+    private void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Collider[] hitColliders = Physics.OverlapSphere(contact.point, radius, layerMask);
+        Instantiate(explosionPrefab, contact.point, Quaternion.identity);
+
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i].gameObject.CompareTag("Enemy") == false)
+            {
+                continue;
+            }
+            hitColliders[i].GetComponent<Enemy>().TakeDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
