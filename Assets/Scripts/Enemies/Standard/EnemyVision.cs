3fceb439d1a0106c02b702b2dce6a5b1d49e37de
oultrox <jesus-perez95@hotmail.com>
2018-02-08 18:02:41 -0300

    [Pol] Enemy vision

--- Assets/Scripts/Enemies/Standard/EnemyVision.cs
+++ Assets/Scripts/Enemies/Standard/EnemyVision.cs
@@ -6,14 +6,28 @@ public class EnemyVision : MonoBehaviour {
 
     Vector3 destination;
     private NavMeshAgent enemyNavMesh;
-
+    private bool movedOnce = false;
     private void Awake()
     {
-        enemyNavMesh = GetComponentInParent<NavMeshAgent>();
+        enemyNavMesh = this.GetComponentInParent<NavMeshAgent>();
     }
+
     public void Update()
     {
-        destination = enemyNavMesh.destination;
+        if (enemyNavMesh.velocity == Vector3.zero && movedOnce == false)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            movedOnce = true;
+            destination = enemyNavMesh.destination;
+        }
         transform.LookAt(destination);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, transform.forward * 10000f);
     }
 }
