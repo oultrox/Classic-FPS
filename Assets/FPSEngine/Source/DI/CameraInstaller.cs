d0d6574d21573981811bdcb365979678078ee837
Jesús Pérez <jesus-perez95@hotmail.com>
2025-08-23 00:11:35 -0400

    Improving injection of components.

---
+++ Assets/FPSEngine/Source/DI/CameraInstaller.cs
@@ -0,0 +1,21 @@
+﻿using DumbInjector;
+using UnityEngine;
+
+public class CameraInstaller : MonoBehaviour, IDependencyProvider
+{
+    [SerializeField] CameraShaker cameraShaker;
+    [SerializeField] DynamicCrosshair guiCrosshair;
+    
+    [Provide]
+    public  CameraShaker ProvideCameraShaker()
+    {
+        return cameraShaker;
+    }
+    
+    [Provide]
+    public  DynamicCrosshair ProvideDynamicCrosshair()
+    {
+        return guiCrosshair;
+    }
+    
+}
